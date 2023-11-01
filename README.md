418a730d8bdd453675611284e30da051fbab6d83
Chia <yangchai2450@gmail.com>
2023-10-31 22:21:36 -0400

    added the shape.test file

---
+++ README.md
@@ -0,0 +1,27 @@
+# svg-logo-generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## Description
+
+    User are able generate a simple logo base on their input through the command line
+
+## Installation
+
+    N/A
+
+## Usage 
+ 
+    The user is able to type in their desire text, shape, and colors
+
+## Walkthrough Video URL
+
+
+
+## Screenshot
+
+![svg-logo-generator](./image/10-oop-homework-demo.png)
+
+## License
+
+    This project is coverd under the MIT license.
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Shape = require('./lib/Shape')
 const Square = require('./lib/Square');
 const Circle = require('./lib/Circle');
 const Triangle = require('./lib/Triangle');
 
 
-
+// creating an command prompt
 inquirer
     .prompt([
         {
@@ -31,8 +30,20 @@ inquirer
             name: 'shapeColor',
         },
     ])
+    // check and make user the user input is 1~3 charcters
     .then((data) => {
         console.log(data)
+        var userInput = "";
+        if (data.text.length > 0 && data.text.length < 4) {
+           
+            userInput = data.text;
+        } else {
+            
+            console.log("Invalid input. The text field can only contain 1 ~3 characters");
+            return;
+        }
+    
+    // check for user selection on the shapes
         let shape;
         if (data.shape == 'Square') {
             shape = new Square(data.text, data.textColor, data.shapeColor)
@@ -44,11 +55,11 @@ inquirer
             shape = new Circle(data.text, data.textColor, data.shapeColor)
         }
         console.log(shape);
+        // create an file call logo.svg
         fs.writeFile('logo.svg', shape.render(), (err) =>
-        err ? console.log(err) : console.log('success! Your SVG logo has been created')
+        err ? console.log(err) : console.log('success! Generated logo.svg')
         );
        
     }); 
 
     
-
